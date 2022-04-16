c1643d1fc624336ddd0519e54feeb7da711a6c9a
{Vlad} <{vladpolikarpovvl@gmail.com}>
2022-04-16 12:59:46 +0300

    first commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../../../../../політех/ооп/2/lab_2.3/lab_2.3/LongLong.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			LongLong l(12, 14);
+			Assert::AreEqual(26.0, l.getOlder() + l.getYounger());
+		}
+	};
+}
---
+++ lab_2.3/lab_2.3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "LongLong.h"
+
+using namespace std;
+
+int main()
+{
+    LongLong l;
+
+    cout << "Enter l : "; cin >> l;
+
+    cout << l << endl;
+
+    cout << "Addition : " << l.getOlder() + l.getYounger() << endl;
+    cout << "Multiplication : " << l.getOlder() * l.getYounger() << endl;
+    cout << "Comparing : ";
+    l.Compare();
+}
